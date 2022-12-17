a4be95397da7262f7bfd9c3324c4124ed898d8e0
kingfeb07 <112013700+kingfeb07@users.noreply.github.com>
2022-12-17 11:57:33 +0700

    2 files modified

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ import { initializeApp } from 'firebase/app';
 
 // Add the Firebase products and methods that you want to use
 import {} from 'firebase/auth';
-import {} from 'firebase/firestore';
+import {
+  getAuth,
+  EmailAuthProvider,
+  signOut,
+  onAuthStateChanged
+} from 'firebase/auth'; 
+import {
+  getFirestore,
+  addDoc,
+  collection
+  
+
+} from 'firebase/firestore';
+import { getAuth, EmailAuthProvider } from 'firebase/auth';
+
 
 import * as firebaseui from 'firebaseui';
 
@@ -27,9 +41,41 @@ let db, auth;
 
 async function main() {
   // Add Firebase project configuration object here
-  const firebaseConfig = {};
+  const firebaseConfig = {
+    apiKey: "AIzaSyAmfDLlv7qtVcCTOTZl8Yy12G5JptUhLWU",
+    authDomain: "fir-web-codelab-8a56a.firebaseapp.com",
+    projectId: "fir-web-codelab-8a56a",
+    storageBucket: "fir-web-codelab-8a56a.appspot.com",
+    messagingSenderId: "236988489100",
+    appId: "1:236988489100:web:3040700b4643545d128aa9"
+
+  };
+  startRsvpButton.addEventListener("click",
+  () => {
+       ui.start("#firebaseui-auth-container", uiConfig);
+ });
 
   // initializeApp(firebaseConfig);
+  initializeApp(firebaseConfig);
+auth = getAuth();
+db = getFirestore();
+
+// Listen to the form submission
+form.addEventListener('submit', async e => {
+  // Prevent the default form redirect
+  e.preventDefault();
+  // Write a new message to the database collection "guestbook"
+  addDoc(collection(db, 'guestbook'), {
+    text: input.value,
+    timestamp: Date.now(),
+    name: auth.currentUser.displayName,
+    userId: auth.currentUser.uid
+  });
+  // clear message input field
+  input.value = '';
+  // Return false to avoid redirect
+  return false;
+});
 
   // FirebaseUI config
   const uiConfig = {
@@ -47,6 +93,36 @@ async function main() {
     },
   };
 
-  // const ui = new firebaseui.auth.AuthUI(auth);
+  const ui = new firebaseui.auth.AuthUI(auth);
+
+    // Listen to the current Auth state
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        startRsvpButton.textContent = 'LOGOUT';
+         // Show guestbook to logged-in users
+    guestbookContainer.style.display = 'block';
+      } else {
+        startRsvpButton.textContent = 'RSVP';
+        // Hide guestbook for non-logged-in users
+    guestbookContainer.style.display = 'none';
+  
+      }
+    });
+
+    // Create query for messages
+  const q = query(collection(db, 'guestbook'), orderBy('timestamp', 'desc'));
+  onSnapshot(q, snaps => {
+    // Reset page
+    guestbook.innerHTML = '';
+    // Loop through documents in database
+    snaps.forEach(doc => {
+      // Create an HTML entry for each document and add it to the chat
+      const entry = document.createElement('p');
+      entry.textContent = doc.data().name + ': ' + doc.data().text;
+      guestbook.appendChild(entry);
+    });
+  });
+
+
 }
 main();
